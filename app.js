acf7b71155ed17341b0e943f566f644ce33cbaa8
Diegosaelices <saelicessaelicesdiego@gmail.com>
2023-09-21 17:26:33 +0200

    volando

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("Hola Mundooo"); //Mensaje general
+console.info("Mensaje informativo"); //Mensaje de informacion
+console.warn("Cuidao chaval"); //Aviso
+console.error("Te has equivocado pringao") //Mensaje de error
+//alert("Alerta Alerta"); //Mensaje en ventana emergente
+//confirm("Corfirmamos bro") //Mensaje en ventana emergente pero con otro boton
+//prompt("Mensaje prompt"); //Mensaje en entana emergente que te permite recoger informacion
+var num=5; //variable tradicional visible en todo el script. No utilizar
+let numero=8; // variabe accesible solo a su scope
+const pi=3.14; //constante accesible a su scope
+/*
+Tipos
+
+number: numero
+String: cadena
+boolean: 
+NaN: not a number
+Undefined: No definido
+Null: nulo
+
+Operaciones aritméticas
+
+Suma: +
+Resta: -
+Multiplicacion: *
+Division: /
+Resto: %
+
+Operaciones de comparacion
+
+< >     
+<= >=
+== !=
+=== !===  (quiere decir que el contenido de una variable es igual a la otra en cuanto al valor que recibe y a su tipo)
+
+*/
